2a13f5fb0ac9f4d4433bf8ba6dae33e29d1eeaae
Johan <johanur.rahman@outlook.com>
2021-10-30 22:49:52 +0600

    Input Validation Mixin File Added

---
+++ src/mixins/validaitonMixin.js
@@ -0,0 +1,19 @@
+export default {
+    methods: {
+        validateInput(value) {
+            if (value === null || value === "") {
+                return this.constructValidationObj(false, null);
+            }
+
+            if (value < 20 || value > 100) {
+                return this.constructValidationObj(true, "Value must be between 20 and 100")
+            }
+
+            return this.constructValidationObj(false, null);
+        },
+
+        constructValidationObj(show, message) {
+            return { show, message };
+        },
+    },
+};
