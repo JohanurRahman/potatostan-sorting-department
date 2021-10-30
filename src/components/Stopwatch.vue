61c2833c32a105013befbe509d00ad2cb585bbba
Johan <johanur.rahman@outlook.com>
2021-10-31 00:43:20 +0600

    Stopwatch template and script code added

--- src/components/Stopwatch.vue
+++ src/components/Stopwatch.vue
@@ -1,10 +1,37 @@
 <template>
-
+  <div id="stopwatch">
+    <p>{{ formattedElapsedTime }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Stopwatch"
+  name: "Stopwatch",
+  data: () => ({
+    elapsedTime: 0,
+    timer: undefined
+  }),
+  computed: {
+    formattedElapsedTime() {
+      const date = new Date(null);
+      date.setSeconds(this.elapsedTime / 1000);
+      const utc = date.toUTCString();
+      return utc.substr(utc.indexOf(":") - 2, 8);
+    },
+  },
+  methods: {
+    start() {
+      this.timer = setInterval(() => {
+        this.elapsedTime += 1000;
+      }, 1000);
+    },
+    stop() {
+      clearInterval(this.timer);
+    },
+    reset() {
+      this.elapsedTime = 0;
+    },
+  },
 }
 </script>
 
