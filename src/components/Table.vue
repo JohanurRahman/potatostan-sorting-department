1ef6b678bc7181c0fb846592e37f565dc6fd1cf6
Johan <johanur.rahman@outlook.com>
2021-10-31 00:16:18 +0600

    Table Component Updated with Props and Template

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,13 +1,39 @@
 <template>
-
+  <v-data-table height="500" :headers="headers"
+                :items="user" :disable-pagination="true"
+                :hide-default-footer="true">
+    <template v-slot:body="props">
+      <draggable :list="props.items" tag="tbody">
+        <tr v-for="(user, index) in props.items" :key="index">
+          <td>{{ user.email }}</td>
+          <td>{{ user.potatoes }}</td>
+          <td>{{ user.name }}</td>
+        </tr>
+      </draggable>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
+import Draggable from "vuedraggable";
+
 export default {
-  name: "Table"
+  name: "Table",
+  components: {
+    Draggable,
+  },
+  props: {
+    user: {
+      default: [],
+      required: true,
+    },
+  },
+  data: () => ({
+    headers: [
+      { text: "Email", value: "email", sortable: false },
+      { text: "Potatoes", value: "potatoes", sortable: false },
+      { text: "Name", value: "name", sortable: false },
+    ],
+  })
 }
 </script>
-
-<style scoped>
-
-</style>
