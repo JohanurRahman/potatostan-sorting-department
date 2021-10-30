f63c5e7da0a67947e5014b2ddad2be6f42dc314c
Johan <johanur.rahman@outlook.com>
2021-10-30 18:56:09 +0600

    Home container css aded

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,7 @@
 <template>
+  <div id="home" class="home-container">
+    <h3>asdsad</h3>
+  </div>
 </template>
 
 <script>
@@ -6,3 +9,12 @@ export default {
   name: 'Home',
 }
 </script>
+
+<style lang="scss" scoped>
+.home-container {
+  max-width: 1140px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 100px 0;
+}
+</style>
